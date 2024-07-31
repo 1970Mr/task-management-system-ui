be05d39a159350a2442afc82d5c3a37a7e756149
1970Mr <mrepoch1970@gmail.com>
2024-07-31 12:56:56 +0430

    Add dashboard view and some components

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <div class="container mx-auto py-6">
+      <TaskList />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Layout/Header.vue";
+import TaskList from "@/components/Dashboard/TaskList.vue";
+
+export default {
+  components: {
+    Header,
+    TaskList,
+  },
+};
+</script>
