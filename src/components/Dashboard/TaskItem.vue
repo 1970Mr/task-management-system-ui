8755311345b544802bc9879091fc88ca359f7dcb
1970Mr <mrepoch1970@gmail.com>
2024-07-31 13:07:10 +0430

    Add TaskList and TaskItem components

---
+++ src/components/Dashboard/TaskItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="bg-white p-4 rounded-lg shadow mb-4 flex justify-between items-center"
+  >
+    <div>
+      <div class="text-lg font-semibold">{{ task.title }}</div>
+      <div class="text-sm text-gray-600">{{ task.description }}</div>
+    </div>
+    <div class="flex space-x-2">
+      <button @click="$emit('edit', task)" class="btn btn-secondary">
+        Edit
+      </button>
+      <button @click="$emit('delete', task.id)" class="btn btn-danger">
+        Delete
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    task: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn-secondary {
+  @apply bg-gray-600 text-white py-1 px-2 rounded-lg hover:bg-gray-700;
+}
+
+.btn-danger {
+  @apply bg-red-600 text-white py-1 px-2 rounded-lg hover:bg-red-700;
+}
+</style>
---
+++ src/components/Dashboard/TaskList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <button @click="openModal" class="btn btn-primary mb-4">Add Task</button>
+    <div v-if="tasks.length === 0">No tasks available.</div>
+    <div v-else>
+      <TaskItem
+        v-for="task in tasks"
+        :key="task.id"
+        :task="task"
+        @edit="editTask"
+        @delete="deleteTask"
+      />
+    </div>
+    <TaskModal
+      v-if="showModal"
+      @close="closeModal"
+      @save="saveTask"
+      :task="selectedTask"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+import TaskItem from "./TaskItem.vue";
+import TaskModal from "./TaskModal.vue";
+
+export default {
+  components: {
+    TaskItem,
+    TaskModal,
+  },
+  data() {
+    return {
+      showModal: false,
+      selectedTask: null,
+    };
+  },
+  computed: {
+    ...mapState("tasks", ["tasks"]),
+  },
+  methods: {
+    ...mapActions("tasks", [
+      "fetchTasks",
+      "addTask",
+      "updateTask",
+      "deleteTask",
+    ]),
+    openModal() {
+      this.selectedTask = null;
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+    editTask(task) {
+      this.selectedTask = task;
+      this.showModal = true;
+    },
+    async saveTask(task) {
+      if (task.id) {
+        await this.updateTask(task);
+      } else {
+        await this.addTask(task);
+      }
+      this.closeModal();
+    },
+  },
+  async created() {
+    await this.fetchTasks();
+  },
+};
+</script>
